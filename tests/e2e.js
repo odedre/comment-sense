8505cf8f59a26de4f32535b69ad737d0d82846a8
Oded Reuveny <oded.reuveny@gmail.com>
2017-05-08 22:18:40 +0300

    first commit

---
+++ tests/e2e.js
@@ -0,0 +1,13 @@
+var test = require('tape')
+
+test('e2e', function (t) {
+  t.plan(1)
+  // var testCase = `
+  //   //identity function
+  //   function firstTest(x) {
+  //     return x;
+  //   }
+  // `
+
+  t.equal(true, true)
+})
