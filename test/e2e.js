3afd572fbba2391a73619ec4e92dedae69a39e31
Doron <qballer@gmail.com>
2017-05-08 22:39:22 +0300

    transpile tests, fix setup

---
+++ test/e2e.js
@@ -0,0 +1,12 @@
+const test = require('tape')
+
+test('e2e', function (t) {
+  t.plan(1)
+  var testCase = `
+    //identity function
+    function firstTest(x) {
+      return x;
+    }
+  `
+  console.log(testCase)
+})
